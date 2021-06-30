58d50bc139e261a24e90d16799b223973d7d6d89
Leekangmin <kkk5474096@naver.com>
2021-06-30 17:23:29 +0900

    [feat] Toast & 서버 통신 확장함수 추가

---
+++ app/src/main/java/team/bum/util/ContextExtension.kt
@@ -0,0 +1,36 @@
+package team.bum.util
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.math.roundToInt
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun <ResponseType> Call<ResponseType>.enqueueUtil(
+    onSuccess: (ResponseType) -> Unit,
+    onError: ((stateCode: Int) -> Unit)? = null
+) {
+    this.enqueue(object : Callback<ResponseType> {
+        override fun onResponse(call: Call<ResponseType>, response: Response<ResponseType>) {
+            if (response.isSuccessful) {
+                onSuccess.invoke(response.body() ?: return)
+            } else {
+                onError?.invoke(response.code())
+            }
+        }
+
+        override fun onFailure(call: Call<ResponseType>, t: Throwable) {
+            Log.d("NetworkTest", "error:$t")
+        }
+
+    })
+}
+
+fun Context.dpToPixel(dp: Int): Int =
+    (dp * resources.displayMetrics.density).roundToInt()

689bea4aabb1f9f818a957eef6e31990141f5e6c
kmebin <blowhuiee@gmail.com>
2021-07-02 14:59:49 +0900

    [modify] visible 함수 사용해서 수정

--- app/src/main/java/team/bum/ui/main/MainActivity.kt
+++ app/src/main/java/team/bum/ui/main/MainActivity.kt
@@ -2,13 +2,14 @@ package team.bum.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 import team.bum.databinding.ActivityMainBinding
 import team.bum.ui.main.archive.ArchiveFragment
 import team.bum.ui.main.collection.CollectionFragment
 import team.bum.ui.main.home.HomeFragment
 import team.bum.ui.main.setting.SettingFragment
+import team.bum.util.setInvisible
+import team.bum.util.setVisible
 
 class MainActivity : AppCompatActivity() {
 
@@ -47,10 +48,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun hideBottomNav() {
-        binding.bottomNavi.visibility = View.GONE
+        binding.bottomNavi.setInvisible()
     }
 
     fun showBottomNav() {
-        binding.bottomNavi.visibility = View.VISIBLE
+        binding.bottomNavi.setVisible()
     }
 }

6ba077f9a8849f95eee19949470cd82715365acc
kmebin <blowhuiee@gmail.com>
2021-07-02 15:13:45 +0900

    [feat] HomeWriting navigation 구현

--- app/src/main/java/team/bum/ui/main/home/HomeFragment.kt
+++ app/src/main/java/team/bum/ui/main/home/HomeFragment.kt
@@ -1,19 +1,29 @@
 package team.bum.ui.main.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import team.bum.R
+import team.bum.databinding.FragmentHomeBinding
+import team.bum.ui.base.BaseFragment
+import team.bum.ui.main.MainActivity
+import team.bum.ui.main.home.writing.HomeWritingFragment
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun initBinding(inflater: LayoutInflater, container: ViewGroup?) =
+        FragmentHomeBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        configureHomeNavigation()
     }
 
+    private fun configureHomeNavigation() {
+        binding.writing.setOnClickListener {
+            (activity as MainActivity).replaceFragment(HomeWritingFragment())
+            (activity as MainActivity).hideBottomNav()
+        }
+    }
 }

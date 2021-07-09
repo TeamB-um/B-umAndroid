4a3e8a8f283ecf87e9e1b15df89ce21ad2c138b2
kmebin <blowhuiee@gmail.com>
2021-07-09 17:13:09 +0900

    [feat] 선택/취소 chip 로직 구현

--- app/src/main/java/team/bum/ui/main/archive/writing/ArchiveWritingFragment.kt
+++ app/src/main/java/team/bum/ui/main/archive/writing/ArchiveWritingFragment.kt
@@ -8,7 +8,11 @@ import androidx.recyclerview.widget.GridLayoutManager
 import team.bum.databinding.FragmentArchiveWritingBinding
 import team.bum.ui.base.BaseFragment
 import team.bum.ui.main.archive.adapter.ArchiveWritingAdapter
+import team.bum.ui.main.archive.adapter.ArchiveWritingAdapter.Companion.MODE_NORMAL
+import team.bum.ui.main.archive.adapter.ArchiveWritingAdapter.Companion.MODE_SELECT
 import team.bum.ui.main.archive.data.ArchiveWritingInfo
+import team.bum.util.setInvisible
+import team.bum.util.setVisible
 
 class ArchiveWritingFragment : BaseFragment<FragmentArchiveWritingBinding>() {
     private val archiveWritingAdapter = ArchiveWritingAdapter()
@@ -17,22 +21,25 @@ class ArchiveWritingFragment : BaseFragment<FragmentArchiveWritingBinding>() {
         FragmentArchiveWritingBinding.inflate(inflater, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         binding.recyclerMywritingList.layoutManager = GridLayoutManager(activity, 2)
         binding.recyclerMywritingList.adapter = archiveWritingAdapter
-
         addArchiveWritingInfo()
-        configureChips()
+        configureWritingList()
     }
 
-    private fun configureChips() {
-        binding.chipSelect.setOnClickListener {
-            binding.chipSelect.apply {
-                text = if (isChecked) "취소" else "선택"
-                archiveWritingAdapter.setViewMode(
-                    if (isChecked) ArchiveWritingAdapter.MODE_SELECT
-                    else ArchiveWritingAdapter.MODE_NORMAL
-                )
+    private fun configureWritingList() {
+        binding.chipSelect.apply {
+            setOnClickListener {
+                if (isChecked) {
+                    text = "취소"
+                    binding.chipRemove.setVisible()
+                    archiveWritingAdapter.setViewMode(MODE_SELECT)
+                } else {
+                    text = "선택"
+                    binding.chipRemove.setInvisible()
+                    archiveWritingAdapter.setViewMode(MODE_NORMAL)
+                    archiveWritingAdapter.clearSelectedItem()
+                }
             }
         }
     }
@@ -42,52 +49,52 @@ class ArchiveWritingFragment : BaseFragment<FragmentArchiveWritingBinding>() {
             listOf<ArchiveWritingInfo>(
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고",
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 ),
                 ArchiveWritingInfo(
                     writingCategory = "인간관계",
-                    writingTitle =  "글제목1",
+                    writingTitle = "글제목1",
                     writingContent = "어쩌고저쩌고"
                 )
             )

026677bd9b3c67104d6fe889a4616b71b4d3a36e
Leekangmin <kkk5474096@naver.com>
2021-07-09 17:29:22 +0900

    [modify] recyclerView 클릭 리스너 추가

--- app/src/main/java/team/bum/ui/main/collection/CollectionFragment.kt
+++ app/src/main/java/team/bum/ui/main/collection/CollectionFragment.kt
@@ -7,10 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import team.bum.R
 import team.bum.databinding.FragmentCollectionBinding
 import team.bum.databinding.FragmentSettingBinding
 import team.bum.ui.base.BaseFragment
+import team.bum.ui.main.MainActivity
 import team.bum.ui.main.collection.adapter.CollectionAdapter
 import team.bum.ui.main.collection.data.CollectionInfo
 
@@ -24,10 +26,12 @@ class CollectionFragment : BaseFragment<FragmentCollectionBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.recyclerCollection.layoutManager = GridLayoutManager(activity,2)
+        binding.recyclerCollection.layoutManager = GridLayoutManager(activity, 2)
         binding.recyclerCollection.adapter = collectionAdapter
 
         addCollectionInfo()
+        recyclerViewClickEvent()
+
     }
 
     private fun addCollectionInfo() {
@@ -67,5 +71,14 @@ class CollectionFragment : BaseFragment<FragmentCollectionBinding>() {
                 )
             )
         )
+    }
+
+    private fun recyclerViewClickEvent() {
+        collectionAdapter.setItemClickListener(object : CollectionAdapter.ItemClickListener {
+            override fun onClick(view: View, position: Int) {
+                (activity as MainActivity).navigateCollectionList()
+            }
+        })
+
     }
 }

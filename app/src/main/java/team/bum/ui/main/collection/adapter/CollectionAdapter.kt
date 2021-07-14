fa15fb7d7110143ad17a5a17be74fe4c07215473
kmebin <blowhuiee@gmail.com>
2021-07-14 19:46:13 +0900

    [modify] 분리수거 textColor 적용

--- app/src/main/java/team/bum/ui/main/collection/adapter/CollectionAdapter.kt
+++ app/src/main/java/team/bum/ui/main/collection/adapter/CollectionAdapter.kt
@@ -1,11 +1,10 @@
 package team.bum.ui.main.collection.adapter
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import team.bum.R
@@ -54,19 +53,20 @@ class CollectionAdapter : RecyclerView.Adapter<CollectionAdapter.CollectionViewH
     class CollectionViewHolder(
         private val binding: ItemCollectionBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-        @SuppressLint("ResourceAsColor")
         fun onBind(categoryInfo: CategoryInfo, context: Context) {
             Glide.with(context).load(categoryInfo.img).into(binding.imageCollection)
             binding.apply {
                 tvCategory.text = categoryInfo.name
                when (categoryInfo.index) {
-                   0 -> tvCategory.setTextColor(R.color.blue_2_main)
-                   1 -> tvCategory.setTextColor(R.color.green_2_main)
-                   2 -> tvCategory.setTextColor(R.color.pink_2_main)
-                   3 -> tvCategory.setTextColor(R.color.blue_4)
-                   4 -> tvCategory.setTextColor(R.color.green_3)
-                   5 -> tvCategory.setTextColor(R.color.green_4)
-                   else -> tvCategory.setTextColor(R.color.black)
+                   0 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.blue_2_main))
+                   1 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.green_3))
+                   2 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.pink_3))
+                   3 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.blue_4))
+                   4 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.green_5))
+                   5 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.green_2_main))
+                   6 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.blue_3))
+                   7 -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.pink_2_main))
+                   else -> tvCategory.setTextColor(ContextCompat.getColor(context, R.color.text_grey))
                }
             }
         }

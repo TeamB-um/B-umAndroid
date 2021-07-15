f9829547b33a4143785c8c3e65af1970596fe0db
kmebin <blowhuiee@gmail.com>
2021-07-15 19:33:42 +0900

    [modify] WritingDialog 카테고리 색 적용

--- app/src/main/java/team/bum/ui/dialog/WritingDialog.kt
+++ app/src/main/java/team/bum/ui/dialog/WritingDialog.kt
@@ -10,9 +10,11 @@ import team.bum.R
 import team.bum.databinding.DialogWritingBinding
 import team.bum.ui.base.BaseDialogFragment
 import team.bum.ui.main.archive.writing.ArchiveWritingFragment.Companion.category
+import team.bum.ui.main.archive.writing.ArchiveWritingFragment.Companion.colorIndex
 import team.bum.ui.main.archive.writing.ArchiveWritingFragment.Companion.content
 import team.bum.ui.main.archive.writing.ArchiveWritingFragment.Companion.date
 import team.bum.ui.main.archive.writing.ArchiveWritingFragment.Companion.title
+import team.bum.util.getColor
 
 class WritingDialog : BaseDialogFragment<DialogWritingBinding>() {
 
@@ -20,13 +22,29 @@ class WritingDialog : BaseDialogFragment<DialogWritingBinding>() {
         DialogWritingBinding.inflate(inflater, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initWritingDialog()
         setupDialogBackground()
+    }
 
-        binding.tvCategory.text = category
-        binding.tvTitle.text = title
-        binding.tvDate.text = date
-        binding.tvContent.text = content
-        binding.imageClose.setOnClickListener { dismiss() }
+    private fun initWritingDialog() {
+        binding.apply {
+            tvCategory.text = category
+            tvTitle.text = title
+            tvDate.text = date
+            tvContent.text = content
+            imageClose.setOnClickListener { dismiss() }
+            when (colorIndex) {
+                0 -> tvCategory.setTextColor(getColor(R.color.blue_2_main))
+                1 -> tvCategory.setTextColor(getColor(R.color.green_3))
+                2 -> tvCategory.setTextColor(getColor(R.color.pink_3))
+                3 -> tvCategory.setTextColor(getColor(R.color.blue_4))
+                4 -> tvCategory.setTextColor(getColor(R.color.green_5))
+                5 -> tvCategory.setTextColor(getColor(R.color.green_2_main))
+                6 -> tvCategory.setTextColor(getColor(R.color.blue_3))
+                7 -> tvCategory.setTextColor(getColor(R.color.pink_2_main))
+                else -> tvCategory.setTextColor(getColor(R.color.text_grey))
+            }
+        }
     }
 
     private fun setupDialogBackground() {
